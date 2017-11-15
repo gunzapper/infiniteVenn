be17c2e7fc0d34ed704f2aa591fe2497ed24e7c5
gunzapper <gunzapper@gmail.com>
2017-11-15 15:29:35 +0100

    first test - simple one - but we start.

---
+++ test_set2net.py
@@ -0,0 +1,9 @@
+'''test for set2net.'''
+
+import set2net as s2n
+
+
+def test_purge_simple():
+    '''basic test'''
+    result = s2n.purge({0, 1, 2, 3, 4, 5}, [{0, 1, 2}, {1, 2, 3}, {2, 3, 4}])
+    assert result == {5}
