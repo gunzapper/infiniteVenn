30b5c63b9336032e21999faee6b796b678c5ddb7
gunzapper <gunzapper@gmail.com>
2017-11-15 15:52:33 +0100

    write a test to cover an hypothesis - no side effect on other_sets parameter in purge

--- test_set2net.py
+++ test_set2net.py
@@ -1,5 +1,7 @@
 '''test for set2net.'''
 
+from copy import deepcopy
+
 import set2net as s2n
 
 
@@ -9,3 +11,15 @@ def test_purge_simple():
     assert result == {5}
     result = s2n.purge({0, 1, 2, 3, 4}, [{0, 1, 2}, {1, 2, 3}, {2, 3, 4}])
     assert result == set()
+
+
+def test_purge_other_sets_no_change():
+    '''testing that at the end of computation other_sets does not change'''
+
+    other_sets = [{0, 1, 2}, {1, 2, 3}, {2, 3, 4}]
+    copy_of_other_sets = deepcopy(other_sets)
+    assert other_sets == copy_of_other_sets
+    assert other_sets is not copy_of_other_sets
+
+    s2n.purge({0, 1, 2, 3, 4, 5}, other_sets)
+    assert other_sets == copy_of_other_sets
